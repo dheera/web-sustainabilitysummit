b319bfb94a26aecdec77757836ea8feada2cce30
Dheera Venkatraman <dheera@dheera.net>
2015-02-19 19:44:18 +0000

    deply

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-rm summit/cache/*
-sync
-rsync -r -u -v --delete --delete-after --exclude 'dbbackup' --exclude 'README.md' --exclude 'htaccess' --exclude '.git' --exclude 'cache' --exclude '.htaccess' --exclude 'deploy.sh' . /mit/sustainability/web_scripts/summit/
+ssh athena.dialup.mit.edu 'cd /afs/athena.mit.edu/org/s/sustainability/web_scripts/summit;git pull'
